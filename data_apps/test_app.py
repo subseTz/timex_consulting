0395fbb068f5793edaeb82b1e311fbf77ace9997
subseTz <felipemirafloreseng@gmail.com>
2024-11-15 23:04:41 -0300

    Update test_app.py

--- data_apps/test_app.py
+++ data_apps/test_app.py
@@ -11,7 +11,9 @@ st.title("Financial Data Analysis")
 # Define the ticker symbol
 tickerSymbol = st.text_input("Enter Ticker Symbol based on Yahoo Finance", "EURUSD=X")
 
-# Get data
+### Daily Data
+#========================================================================================
+# Get Daily data
 tickerData = yf.Ticker(tickerSymbol)
 df_1d = tickerData.history(period='2y', interval='1d')
 
@@ -19,7 +21,7 @@ df_1d = tickerData.history(period='2y', interval='1d')
 df_1d = df_1d.drop(['Volume', 'Dividends', 'Stock Splits'], axis=1)
 
 # --- Input for SMA and Bollinger Bands ---
-sma_window = st.number_input("SMA Window", min_value=20, value=20)
+sma_window = st.number_input("SMA Window", min_value=20, value=50)
 bb_window = st.number_input("Bollinger Bands Window", min_value=20, value=50)
 
 # Calculate SMA 
@@ -68,7 +70,7 @@ fig.add_trace(go.Scatter(x=df_1d_plot.index, y=df_1d_plot[f'lower_ci{bb_window}'
 
 # Signals (using the selected SMA window)
 df_1d['position'] = 'NONE'  # Initialize POSITION column
-parameters = [1.618, 2, 3]
+parameters = [2, 3]
 
 for i in parameters:
     for j in range(len(df_1d_plot)):
@@ -81,7 +83,7 @@ for i in parameters:
 
 # Update layout
 fig.update_layout(
-    title=f"Daily (1D) Time Series of {tickerSymbol}",
+    title=f"Timex Z-Score Mean Reversal Model for {tickerSymbol}",
     xaxis_title="Date",
     yaxis_title="Close Price",
     xaxis_rangeslider_visible=True  # Add a range slider for zooming
@@ -90,6 +92,91 @@ fig.update_layout(
 # Display the chart in Streamlit
 st.plotly_chart(fig, use_container_width=True)
 
+
+### Hourly Data
+#========================================================================================
+# Get Hourly data
+tickerData_1h = yf.Ticker(tickerSymbol)
+df_1h = tickerData.history(period='2y', interval='1h')
+
+# Drop unnecessary columns
+df_1h = df_1h.drop(['Volume', 'Dividends', 'Stock Splits'], axis=1)
+
+# --- Input for SMA and Bollinger Bands ---
+sma_window_1h_1h = st.number_input("SMA Window", min_value=20, value=50)
+bb_window_1h = st.number_input("Bollinger Bands Window", min_value=20, value=50)
+
+# Calculate SMA 
+df_1h[f'sma{sma_window_1h_1h}'] = df_1h['Close'].rolling(window=sma_window_1h).mean() 
+
+# Calculate returns as var (%)
+df_1h["returns"] = df_1h["Close"].pct_change() * 100
+df_1h[f"returns_sma{sma_window_1h_1h}"] = (1 - (df_1h['Close'] / df_1h[f'sma{sma_window_1h_1h}'])) * 100
+
+# Z-Scores
+df_1h['zscores_close'] = (df_1h['returns'] - df_1h['returns'].mean()) / df_1h['returns'].std()
+df_1h[f'zscores_sma{sma_window_1h}'] = (df_1h[f'returns_sma{sma_window_1h}'] - df_1h[f'returns_sma{sma_window_1h}'].mean()) / df_1h[f'returns_sma{sma_window_1h}'].std()
+
+# Remove NaN values
+df_1h.dropna(subset=["returns", f'returns_sma{sma_window_1h}'], inplace=True)
+
+# --- Calculate Bollinger Bands ---
+df_1h[f'sma{bb_window}'] = df_1h['Close'].rolling(window=bb_window).mean()
+df_1h[f'std{bb_window}'] = df_1h['Close'].rolling(window=bb_window).std()
+df_1h[f'upper_ci{bb_window}'] = df_1h[f'sma{bb_window}'] + (2 * df_1h[f'std{bb_window}'])
+df_1h[f'lower_ci{bb_window}'] = df_1h[f'sma{bb_window}'] - (2 * df_1h[f'std{bb_window}'])
+
+# --- Plotting with Plotly ---
+
+# Filter data for the past year for plotting
+df_1h_plot = df_1h.copy()
+
+# Create Plotly figure
+fig = go.Figure()
+
+# Close price line
+fig.add_trace(go.Scatter(x=df_1h_plot.index, y=df_1h_plot['Close'], 
+                         mode='lines', name='Close', line=dict(color='blue')))
+
+# SMA line
+fig.add_trace(go.Scatter(x=df_1h_plot.index, y=df_1h_plot[f'sma{sma_window_1h}'], 
+                         mode='lines', name=f'SMA {sma_window_1h}', line=dict(color='orange')))
+
+# Bollinger Bands
+fig.add_trace(go.Scatter(x=df_1h_plot.index, y=df_1h_plot[f'upper_ci{bb_window}'], 
+                         mode='lines', name=f'Upper Bollinger Band ({bb_window})', 
+                         line=dict(color='gray', width=1)))
+fig.add_trace(go.Scatter(x=df_1h_plot.index, y=df_1h_plot[f'lower_ci{bb_window}'], 
+                         mode='lines', name=f'Lower Bollinger Band ({bb_window})', 
+                         line=dict(color='gray', width=1)))
+
+# Signals (using the selected SMA window)
+df_1h['position'] = 'NONE'  # Initialize POSITION column
+parameters = [2, 3]
+
+for i in parameters:
+    for j in range(len(df_1h_plot)):
+        if df_1h_plot[f'zscores_sma{sma_window_1h}'].iloc[j] < -i:  # Short
+            fig.add_vline(x=df_1h_plot.index[j], line_width=0.7, line_dash="dash", line_color="red", opacity=0.5)
+            df_1h['position'].loc[df_1h_plot.index[j]] = 'SHORT'
+        if df_1h_plot[f'zscores_sma{sma_window_1h}'].iloc[j] > i:  # Long
+            fig.add_vline(x=df_1h_plot.index[j], line_width=0.7, line_dash="dash", line_color="green", opacity=0.5)
+            df_1h['position'].loc[df_1h_plot.index[j]] = 'LONG'
+
+# Update layout
+fig.update_layout(
+    title=f"Timex Z-Score Mean Reversal Model for {tickerSymbol} - 1H",
+    xaxis_title="Date",
+    yaxis_title="Close Price",
+    xaxis_rangeslider_visible=True  # Add a range slider for zooming
+)
+
+
+
+
+
+
 # Display the DataFrame in Streamlit
 df_st = df_1d[['Open', 'High', 'Low', 'Close', 'returns', f'zscores_sma{sma_window}', 'position']]
+st.write(f"Dataframe of Daily (1D) values of {tickerSymbol}")
 st.write(df_st.sort_index(ascending=False))
